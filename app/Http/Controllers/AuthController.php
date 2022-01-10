4b510e24adf4a99cb887393273d89e47f0de1d2c
Fernando Queiroz Acquesta <facquesta5@gmail.com>
2022-01-10 19:19:12 -0300

    Corrigir autenticação por formulário

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,8 +11,6 @@ class AuthController extends Controller
 {
     public function login()
     {
-
-        dd(Auth::user());
         return view('login');
     }
 
@@ -29,26 +27,20 @@ class AuthController extends Controller
             ]
         ]);
 
-        if (!Auth::attempt($credenciais)) { // Se a collection $dadosUsuario estiver vazia, retorna 0
-
-            return response()->json(false, 401);
+        if (!Auth::attempt($credenciais)) { // com ajax* Se a collection $credenciais estiver vazia, retorna ...
+            return redirect()->back(); //força usuario a voltar para o lugar de onde veio
+            //response()->json(false, 401);
         }
 
-        $status = [
-            1 => 'home/admin',
-            2 => 'home/funcionario',
-        ];
-        return response()->json([
-            // 'status' => $status[$dadosUsuario->nivel_id],
-            // 'token' => csrf_token()
-        ]);
+        $route = Auth::user()->nivel_id == '1' ? route('home.admin') : route('home.funcionario');
+
+        return redirect($route);
     }
 
     public function logout()
     {
+        Auth::logout();//Função para cuidar de remover a sessão de login do usuario
 
-        Session::flush();
-
-        return redirect(route('login'));
+        return redirect()->route('login');
     }
 }
